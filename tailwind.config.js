b4a07f77d4f65ba6fee1c2a95a2b2d4cefe0aaa9
hg-jang <wkdgusrhkd@gmail.com>
2022-11-20 20:34:28 +0900

    Tailwind 설정

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
-/** @type { import('tailwindcss').Config } */ 
+/** @type {import('tailwindcss').Config} */ 
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}"
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
