1a5322ce64d2d78b4784808018c4e2252f7cf7eb
hg-jang <wkdgusrhkd@gmail.com>
2022-12-18 17:41:08 +0900

    next proxy pass 추가

--- front/next.config.js
+++ front/next.config.js
@@ -2,6 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  async rewrites() {
+    return [
+      {
+        source: '/back/api/:path*',
+        destination: 'http://localhost:8080/back/api/:path*'
+      }
+    ]
+  },
 }
 
 module.exports = nextConfig
